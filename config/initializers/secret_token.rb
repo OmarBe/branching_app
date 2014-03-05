b876aeb9b4ddf149e5bff8820bbe9e6d69c0ee67
Omar Benitez <oomaar@live.com.mx>
2014-03-05 16:21:12 -0700

    Comit inicial.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BranchingApp::Application.config.secret_token = '58b43650f1330918321700111f56d67308ff4b2cd33a55e26f0553635170c5f1357f1cf806a83e1d6082176a9bd9391193be8b391adef262133227d09e8fcd3c'
